e901849142e90051d8058dc08745c742ab1b8551
DRyzhkovProd <dryzhkov98@yandex.ru>
2022-01-04 21:49:50 +0300

    add env config and path to env file in package.json, clear app.js. Add env configs to db.js

--- server/app.js
+++ server/app.js
@@ -1,19 +1,24 @@
 import express from 'express'
-import dotenv from 'dotenv'
 import sequelize from "./db.js";
 import * as models from './models/models.js'
 import cors from 'cors'
-dotenv.config()
 
-const PORT= process.env.PORT || 5000
+
+
+
+const PORT= process.env.PORT
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 const start = async () => {
-    try{
+    try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, ()=> console.log(`server started on ${PORT} port`))
-    } catch (e) {}
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
 }
+
+start()
--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
 import {Sequelize} from "sequelize";
-
- const sequelize = new Sequelize(
-    process.env.POSTGRESS_NAME,
-    process.env.POSTGRESS_USER,
-    process.env.POSTGRESS_PASSWORD,
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    '123',
     {
         dialect: 'postgres',
-        host: process.env.POSTGRESS_HOST,
+        host: process.env.DB_HOST,
         port: process.env.POSTGRESS_PORT
     }
 )
